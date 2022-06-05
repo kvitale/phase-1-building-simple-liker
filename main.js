85b6ed7e89fa251d11f378ac279eed5f5d9a1f19
Kyle <ktvitale7@gmail.com>
2022-06-05 11:26:07 -0400

    commit tree 8703262d3b8d0ab5685398893b94eb76e504e872

--- main.js
+++ main.js
@@ -2,7 +2,44 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+const likeHearts = document.querySelectorAll('.like-glyph')
+console.log(likeHearts)
+
+likeHearts.forEach((heart) => {
+  heart.addEventListener('click', e => {
+    mimicServerCall()
+      .then(data => {
+
+        if(heart.textContent === FULL_HEART){
+          heart.textContent = EMPTY_HEART
+          
+        }
+        else{heart.textContent = FULL_HEART
+          heart.className = "activated-heart"}
+      })
+      .catch(resp => {
+        document.querySelector('div').className = ''
+        setTimeout(() => document.querySelector('div').className = 'hidden',(3000))
+          
+        })
+
+        
+        
+      })
+      
+    })
+    
+
+
 // Your JavaScript code goes here!
+// Add hidden class error to modal 
+// const hiddenClass = document.querySelector('div')
+// hiddenClass.className = 'hidden'
+// click on empty heart Invoke/mimicSeverCall 
+// function likeCallBack(e){
+//   const heart = e.target
+//   console.log(heart)
+// }
 
 
 
